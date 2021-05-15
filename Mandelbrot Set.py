1373881acb8e4825db69061a8d0cd2c614317d76
Akeel Ather Medina <56905673+AkeelMedina22@users.noreply.github.com>
2021-05-15 14:48:35 +0500

    Update Mandelbrot Set.py

--- Mandelbrot Set.py
+++ Mandelbrot Set.py
@@ -20,3 +20,33 @@ plt.gca().set_aspect('equal')
 plt.imshow(mask.T, extent=[-1.5, 1.5, -1.5, 1.5], cmap='magma')
 
 plt.show()
+
+############################################################################
+
+# Alternate approach (cooler result)
+
+x = np.linspace(-1.2-0.15, -1.2+0.15, 2000).reshape((1, 2000))
+y = np.linspace(0.35-0.15, 0.35+0.15, 2000).reshape((2000, 1))
+c = x + 1j*y
+z = x + 1j*y
+
+n = 512
+
+c = np.full(z.shape, c)
+
+diverge = np.zeros(z.shape)
+
+m = np.full(c.shape, True)
+
+for i in range(n):
+    z[m] = z[m]**2 + c[m]
+    m[np.abs(z) > 2] = False
+    diverge[m] = i
+upper = 1.2
+lower = 0.35
+plt.imshow(diverge, cmap='magma', origin='lower')
+
+plt.show()
+
+# To save the image:
+# plt.savefig("mandlebrot.png", dpi=1200, orientation='landscape')
